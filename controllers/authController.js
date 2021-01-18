c9b143e8c9f5dd1cd4d007f8c3aae0f0c48268a5
Neelesh <neeleshsingh767@gmail.com>
2021-01-18 09:37:28 +0530

    changed cookie settings

--- controllers/authController.js
+++ controllers/authController.js
@@ -11,6 +11,8 @@ const sendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
   const cookieOptions = {
     expires: new Date(Date.now() + 9 * 24 * 60 * 60 * 1000),
+    secure: true,
+    httpOnly: false,
   };
   res.cookie('jwt', token, cookieOptions);
   user.password = undefined;
